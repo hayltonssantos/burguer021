ccf22595ee0afeedfe910363c893e73da60611eb
Haylton Santos <aylton.souza10@gmail.com>
2024-03-30 01:49:23 +0000

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/home/Home'
 import { Route, Routes, BrowserRouter, Navigate, Link } from 'react-router-dom'
 import Foods from './pages/foods/Foods'
 import { HamburguersProvider } from './context/hamburguers'
+import DescFood from './pages/descFood/DescFood'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <HamburguersProvider>
       <BrowserRouter>
         <Routes>
           <Route path='home' element={<Home/>}/>
           <Route path='foods' element={<Foods/>}/>
+          <Route path='/food/:name' element={<DescFood/>}/>
           <Route path='*' element={<Home/>}/>
           <Route path='/' element={<Home/>}/>
         </Routes>
--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,27 +1,62 @@
-import React from 'react'
-import styles from './Cards.module.css'
-import photo from '../../assets/photos/Hamburguer.png'
+import React from 'react';
+import styles from './Cards.module.css';
+import stylesNew from './CardsNew.module.css';
+import photo from '../../assets/photos/Hamburguer.png';
 
+export default function Cards({ food, type = '' }) {
+  if (!food) {
+    return null;
+  }
 
-export default function Cards({food}) {
+  let card = '';
+  let imgBox = '';
+  let mouse = '';
+  let contentBox = '';
+  let price = '';
+  let buy = '';
+
+  if (type === 'newCard') {
+    card = stylesNew.card;
+    imgBox = stylesNew.imgBox;
+    mouse = stylesNew.mouse;
+    contentBox = stylesNew.contentBox;
+    price = stylesNew.price;
+    buy = stylesNew.buy;
+  } else {
+    card = styles.card;
+    imgBox = styles.imgBox;
+    mouse = styles.mouse;
+    contentBox = styles.contentBox;
+    price = styles.price;
+    buy = styles.buy;
+  }
+
   const prices = String(food.price);
-  const parts = prices.split(".");
+  const parts = prices.split('.');
 
-  const parteInteira = parts[0]; 
+  const parteInteira = parts[0];
   const parteDecimal = parts[1];
+
   return (
-    <>
-    <div className={styles.card}>
-      <div className={styles.imgBox}>
-        <img src={photo} alt={`Hamburguer ${food.name}`} className={styles.mouse}/>
+    <div className={card}>
+      <div className={imgBox}>
+        <img src={photo} alt={`Hamburguer ${food.name}`} className={mouse} />
       </div>
-      <div className={styles.contentBox}>
-        <h3>{food.name}</h3>
-        <h4>{food.desc}</h4>
-        <h2 className={styles.price}>{parteInteira}.<small>{parteDecimal}</small> €</h2>
-        <a href="#" className={styles.buy}>Buy Now</a>
-      </div>
+      {type !== 'newCard' ? (
+        <div className={contentBox}>
+          <h3>{food.name}</h3>
+          <h4>{food.desc}</h4>
+          <h2 className={price}>
+            {parteInteira}.<small>{parteDecimal}</small> €
+          </h2>
+
+          <a href={`/food/${food.name}`} key={food.name} className={buy}>
+            Buy Now
+          </a>
+        </div>  
+        ) : (
+          ''
+        )}
     </div>
-    </>
-  )
+  );
 }
